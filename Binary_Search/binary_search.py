1762e3581bbf0e6fc12efae4cccdaf4de3795275
BlueBoi904 <cliff.mirschel@gmail.com>
2022-09-22 20:20:44 -0400

    Added solutions

--- Binary_Search/binary_search.py
+++ Binary_Search/binary_search.py
@@ -28,5 +28,20 @@ nums is sorted in ascending order.
 """
 
 
-def search(nums, list):
-    pass
+def search(nums, target):
+    if len(nums) == 0:
+        return
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        curr = nums[mid]
+
+        if curr == target:
+            return mid
+        elif curr > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1
