fd9366ee89f78b80c742f60ea0abfaebeeeaf385
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-10-01 00:26:20 -0700

    Adding solutions

--- Binary_Search/binary_search_II.py
+++ Binary_Search/binary_search_II.py
@@ -28,5 +28,19 @@ nums is sorted in ascending order.
 """
 
 
-def binary_search(nums):
-    pass
+def binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] < target:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+
+    return -1
