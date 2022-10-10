8e4a87750edeb9a576e38df9b834c83f151c53e1
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-10-10 15:25:56 -0700

    Adding solutions

--- String/permutations.py
+++ String/permutations.py
@@ -28,11 +28,28 @@ from collections import Counter
 
 
 def check_inclusion(s1, s2):
-    s1_count = Counter(s1)
-    s2_count = Counter(s2)
-    print(s1_count)
-    print(s2_count)
-    pass
+    n, m = len(s1), len(s2)
+
+    if n > m:
+        return False
+
+    target_hash = sum(hash(char) for char in s1)
+    cur_hash = sum(hash(char) for char in s2[:n])
+
+    if cur_hash == target_hash:
+        return True
+
+    # sliding window
+    for right in range(n, m):
+
+        # remove the pass char and add the new char
+        left = right - n
+        cur_hash += hash(s2[right]) - hash(s2[left])
+
+        if cur_hash == target_hash:
+            return True
+
+    return False
 
 
 check_inclusion("ab", "eidbaooo")
