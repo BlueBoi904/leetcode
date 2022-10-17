bb61cb8a830c38ad2ec76788f555091e4cd02557
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-10-16 23:32:59 -0700

    Adding solutions

--- Tree/minimum_depth_binary_tree.py
+++ Tree/minimum_depth_binary_tree.py
@@ -30,6 +30,50 @@ The number of nodes in the tree is in the range [0, 105].
 
 """
 
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        children = [root.left, root.right]
+        # if we're at leaf node
+        if not any(children):
+            return 1
+
+        min_depth = float('inf')
+        for c in children:
+            if c:
+                min_depth = min(self.minDepth(c), min_depth)
+        return min_depth + 1
+
 
 def min_depth(root):
-    pass
+    if not root:
+        return 0
+    else:
+        stack, min_depth = [(1, root), ], float('inf')
+
+    while stack:
+        depth, root = stack.pop()
+        children = [root.left, root.right]
+        if not any(children):
+            min_depth = min(depth, min_depth)
+        for c in children:
+            if c:
+                stack.append((depth + 1, c))
+
+    return min_depth
