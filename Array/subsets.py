2f1f5244d9984d96c45ccff12c81dff1484b9b5e
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-10-31 10:37:38 -0700

    Adding solutions

---
+++ Array/subsets.py
@@ -0,0 +1,36 @@
+"""
+
+Given an integer array nums of unique elements, return all possible
+subsets
+(the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+
+"""
+
+
+def subsets(nums):
+    # Get all combinations of arr
+    n = len(nums)
+    output = [[]]
+
+    for num in nums:
+        output += [curr + [num] for curr in output]
+
+    return output
+
+
+subsets([1, 2, 3])  # => [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+subsets([0])  # => [[],[0]]
