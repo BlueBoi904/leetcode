da2baf6940d059ab6d332bb83ad9290da8d47139
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-11-02 10:56:46 -0700

    Adding solutions

--- String/number_of_substrings_containing_all_chars.py
+++ String/number_of_substrings_containing_all_chars.py
@@ -32,26 +32,17 @@ s only consists of a, b or c characters.
 
 class Solution:
     def number_of_sub_strings(self, s: str) -> int:
-        a = b = c = 0                        # counter for letter a/b/c
-        ans, i, n = 0, 0, len(s)             # i: slow pointer
-        for j, letter in enumerate(s):       # j: fast pointer
-            if letter == 'a':
-                a += 1         # increment a/b/c accordingly
-            elif letter == 'b':
-                b += 1
-            else:
-                c += 1
-            while a > 0 and b > 0 and c > 0:  # if all of a/b/c are contained, move slow pointer
-                # count possible substr, if a substr ends at j, then there are n-j substrs to the right that are containing all a/b/c
-                ans += n-j
-                if s[i] == 'a':
-                    a -= 1       # decrement counter accordingly
-                elif s[i] == 'b':
-                    b -= 1
-                else:
-                    c -= 1
-                i += 1                       # move slow pointer
-        return ans
+        results = 0
+        letters = defaultdict(int)
+        l = 0
+        for r in range(len(s)):
+            letters[s[r]] += 1
+            while len(letters.keys()) == 3:
+                letters[s[l]] -= 1
+                if letters[s[l]] == 0:
+                    del letters[s[l]]
+                results += len(s) - r
+                l += 1
+        return results
 
-
-print(number_of_sub_strings("abcabc"))  # => 10
+        return count
