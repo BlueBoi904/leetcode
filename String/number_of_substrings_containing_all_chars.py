56e989c829d07bc9387deec99f58b88f59772504
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-11-02 10:53:30 -0700

    Adding solutions

--- String/number_of_substrings_containing_all_chars.py
+++ String/number_of_substrings_containing_all_chars.py
@@ -29,21 +29,29 @@ s only consists of a, b or c characters.
 
 """
 
-import collections
-
 
 class Solution:
-    def numberOfSubstrings(self, s: str) -> int:
-        j, result, counter = 0, 0, collections.Counter()
-        for i, c in enumerate(s):
-            counter[c] += 1
-            while len(counter) == 3:
-                result += len(s) - i
-                counter[s[j]] -= 1
-                if not counter[s[j]]:
-                    del counter[s[j]]
-                j += 1
-        return result
+    def number_of_sub_strings(self, s: str) -> int:
+        a = b = c = 0                        # counter for letter a/b/c
+        ans, i, n = 0, 0, len(s)             # i: slow pointer
+        for j, letter in enumerate(s):       # j: fast pointer
+            if letter == 'a':
+                a += 1         # increment a/b/c accordingly
+            elif letter == 'b':
+                b += 1
+            else:
+                c += 1
+            while a > 0 and b > 0 and c > 0:  # if all of a/b/c are contained, move slow pointer
+                # count possible substr, if a substr ends at j, then there are n-j substrs to the right that are containing all a/b/c
+                ans += n-j
+                if s[i] == 'a':
+                    a -= 1       # decrement counter accordingly
+                elif s[i] == 'b':
+                    b -= 1
+                else:
+                    c -= 1
+                i += 1                       # move slow pointer
+        return ans
 
 
 print(number_of_sub_strings("abcabc"))  # => 10
