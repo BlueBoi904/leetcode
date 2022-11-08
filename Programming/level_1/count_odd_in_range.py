cbd7d52272db11ede9f37599200639d138739769
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-11-07 22:02:55 -0700

    Adding problem and solution

---
+++ Programming/level_1/count_odd_in_range.py
@@ -0,0 +1,58 @@
+"""
+
+1523. Count Odd Numbers in an Interval Range
+Easy
+1.2K
+82
+company
+Amazon
+company
+Adobe
+company
+Accenture
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+ 
+
+Example 1:
+
+Input: low = 3, high = 7
+Output: 3
+Explanation: The odd numbers between 3 and 7 are [3,5,7].
+Example 2:
+
+Input: low = 8, high = 10
+Output: 1
+Explanation: The odd numbers between 8 and 10 are [9].
+ 
+
+Constraints:
+
+0 <= low <= high <= 10^9
+Accepted
+153.9K
+Submissions
+332.4K
+Acceptance Rate
+46.3%
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Related Topics
+
+"""
+
+
+class Solution:
+
+    def countOdds(self, low: int, high: int) -> int:
+        odd_count = 0
+
+        for i in range(low, high + 1):
+
+            if i % 2 != 0:
+
+                odd_count += 1
+
+        return odd_count
