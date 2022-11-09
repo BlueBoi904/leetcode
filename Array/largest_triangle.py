7633b53ad47dc8cae8e553f008a525226ec4ad6e
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-11-09 11:17:30 -0700

    Adding problems

---
+++ Array/largest_triangle.py
@@ -0,0 +1,49 @@
+"""
+
+976. Largest Perimeter Triangle
+Easy
+2.4K
+337
+company
+Apple
+company
+Tesla
+company
+Adobe
+Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+
+
+Example 1:
+
+Input: nums = [2,1,2]
+Output: 5
+Example 2:
+
+Input: nums = [1,2,1]
+Output: 0
+
+
+Constraints:
+
+3 <= nums.length <= 104
+1 <= nums[i] <= 106
+Accepted
+177.3K
+Submissions
+324.8K
+Acceptance Rate
+54.6%
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Similar Questions
+Related Topics
+
+"""
+
+
+class Solution:
+    def largestPerimeter(self, nums) -> int:
+        pass
