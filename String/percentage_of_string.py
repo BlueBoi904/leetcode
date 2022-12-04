664f7fe410cc1bac7d0b20011af805ba13585870
Cliff Mirschel <cliff.mirschel@gmail.com>
2022-12-03 18:17:22 -0700

    adding leetcode

---
+++ String/percentage_of_string.py
@@ -0,0 +1,41 @@
+"""
+
+2278. Percentage of Letter in String
+Easy
+278
+32
+company
+American Express
+Given a string s and a character letter, return the percentage of characters in s that equal letter rounded down to the nearest whole percent.
+
+
+
+Example 1:
+
+Input: s = "foobar", letter = "o"
+Output: 33
+Explanation:
+The percentage of characters in s that equal the letter 'o' is 2 / 6 * 100% = 33% when rounded down, so we return 33.
+Example 2:
+
+Input: s = "jjjj", letter = "k"
+Output: 0
+Explanation:
+The percentage of characters in s that equal the letter 'k' is 0%, so we return 0.
+
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of lowercase English letters.
+letter is a lowercase English letter.
+
+"""
+
+
+def percentage_letter(s: str, letter: str):
+    return int(s.count(letter) / len(s) * 100)
+
+
+print(percentage_letter("foobar", 'o'))
+print(percentage_letter("jjjj", "k"))
