a4b5c2d891b07e318167130ebcfbbf067fbf49d6
BlueBoi904 <cliff.mirschel@gmail.com>
2023-01-02 14:35:07 -0700

    Adding daily problem

---
+++ String/detect_capital.py
@@ -0,0 +1,30 @@
+"""
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+
+
+Example 1:
+
+Input: word = "USA"
+Output: true
+Example 2:
+
+Input: word = "FlaG"
+Output: false
+
+"""
+
+
+def detect_capital(word: str):
+    pass
+
+
+print(detect_capital("USA"))  # => True
+
+print(detect_capital("FlaG"))  # => False
