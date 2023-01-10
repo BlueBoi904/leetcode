3a123dafaca4d035973546f4e86b34f380572af4
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-01-10 12:59:29 -0700

    adding daily problem

---
+++ Array/build_permutations.py
@@ -0,0 +1,13 @@
+"""
+
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+
+
+"""
+
+
+def build_array(nums: list[int]):
+    return [nums[num] for num in nums]
