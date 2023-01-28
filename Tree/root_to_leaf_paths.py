5c34a8fc1f6e4fe41462ed1df50b8109a80e332f
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-01-28 13:41:43 -0700

    Adding another problem

---
+++ Tree/root_to_leaf_paths.py
@@ -0,0 +1,40 @@
+"""
+
+257. Binary Tree Paths
+Easy
+5.4K
+231
+company
+Amazon
+company
+Capital One
+company
+Bloomberg
+Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+Example 2:
+
+Input: root = [1]
+Output: ["1"]
+
+"""
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def binaryTreePaths(root: TreeNode):
+    pass
