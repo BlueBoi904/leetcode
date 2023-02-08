d9574f66c285e45b59447cacb26843cafd6a7115
BlueBoi904 <cliff.mirschel@gmail.com>
2023-02-08 13:29:14 -0700

    Got another today, lets go!!!

---
+++ Array/longest_consecutive_char.py
@@ -0,0 +1,44 @@
+"""
+
+1446. Consecutive Characters
+Easy
+1.5K
+29
+company
+Apple
+company
+Google
+The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+Given a string s, return the power of s.
+
+
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+Example 2:
+
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of only lowercase English letters.
+Accepted
+135.6K
+Submissions
+221K
+Acceptance Rate
+61.4%
+
+"""
+
+class Solution:
+    def maxPower(self, s: str) -> int:
+        pass
