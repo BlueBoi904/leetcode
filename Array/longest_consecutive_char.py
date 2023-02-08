39df841ab557ba15077a71d389b166d574ad3b5c
BlueBoi904 <cliff.mirschel@gmail.com>
2023-02-08 13:45:21 -0700

    Got another

--- Array/longest_consecutive_char.py
+++ Array/longest_consecutive_char.py
@@ -41,4 +41,16 @@ Acceptance Rate
 
 class Solution:
     def maxPower(self, s: str) -> int:
-        pass
+        count = 0
+        max_count = 0
+        previous = None
+        for c in s:
+            if c == previous:
+                # if same as previous one, increase the count
+                count += 1
+            else:
+                # else, reset the count
+                previous = c
+                count = 1
+            max_count = max(max_count, count)
+        return max_count
