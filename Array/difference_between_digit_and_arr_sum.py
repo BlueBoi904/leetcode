60a46bf0edf1bc7d9d4a9c4fcd4e95c9d31ff1f0
BlueBoi904 <cliff.mirschel@gmail.com>
2023-02-08 12:24:51 -0700

    Got another today!!

--- Array/difference_between_digit_and_arr_sum.py
+++ Array/difference_between_digit_and_arr_sum.py
@@ -34,7 +34,22 @@ Constraints:
 1 <= nums[i] <= 2000
 
 """
+# First attempt, simple solution success!
+
+def differenceOfSum(nums: list[int]) -> int:
+    element_sum = sum(nums)
+    digits = []
+    for num in nums:
+        for digit in str(num):
+            digits.append(int(digit))
+    return element_sum - sum(digits)
+
+print(differenceOfSum([1,15,6,3]))
+
+print(differenceOfSum([1,2,3,4]))
+
+# Faster solution
 
 class Solution:
-    def differenceOfSum(nums: List[int]) -> int:
-        pass
+    def differenceOfSum(self, nums: List[int]) -> int:
+        return sum(map(lambda n: n - sum(map(int, str(n))), nums))
