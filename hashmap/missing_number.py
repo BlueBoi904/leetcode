902223987ae3709c0462bbe1abf1e886c6285d1f
BlueBoi904 <cliff.mirschel@gmail.com>
2023-02-25 23:38:58 -0700

    Added another!!

--- hashmap/missing_number.py
+++ hashmap/missing_number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+    def missingNumber(self, nums: list[int]) -> int:
         for i in range(0, len(nums) + 1):
             if i not in nums:
                 return i
