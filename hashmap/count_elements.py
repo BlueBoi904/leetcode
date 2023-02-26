cc0af2399de6b349abd07225c82f87ddefeccd06
BlueBoi904 <cliff.mirschel@gmail.com>
2023-02-26 15:15:34 -0700

    Added solution!!

--- hashmap/count_elements.py
+++ hashmap/count_elements.py
@@ -2,3 +2,12 @@ class Solution:
     def countElements(self, arr: List[int]) -> int:
         count = collections.Counter(arr)
         return sum(v for k, v in count.items() if count[k + 1])
+
+
+def count_elements(arr: list[int]):
+    count = 0
+    hash_set = set(arr)
+    for x in arr:
+        if (x + 1) in hash_set:
+            count += 1
+    return count
