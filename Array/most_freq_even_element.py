728d307778b0724aead82363501c2cd1c957967b
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-04-06 10:05:44 -0700

    Added daily problems

--- Array/most_freq_even_element.py
+++ Array/most_freq_even_element.py
@@ -1,6 +1,6 @@
 class Solution:
     def mostFrequentEven(self, nums: list[int]) -> int:
-        
+
         nums = [n for n in nums if n % 2 == 0]
         nums.sort()
-        return max(nums,key=nums.count) if len(nums) > 0 else -1
+        return max(nums, key=nums.count) if len(nums) > 0 else -1
