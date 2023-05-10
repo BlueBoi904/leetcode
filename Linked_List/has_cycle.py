3e8761769c6c8679528d976864e3e6a463d491dc
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-05-10 13:13:32 -0700

    Got another!

--- Linked_List/has_cycle.py
+++ Linked_List/has_cycle.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+# First attempt, caching
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         cache = {}
@@ -16,3 +16,18 @@ class Solution:
             cache[curr] = True
 
         return False
+
+
+# Optimized solution
+
+
+def hasCycle(head: Optional[ListNode]) -> bool:
+    node_1, node_2 = head, head
+
+    while node_2 and node_2.next:
+        node_1 = node_1.next
+        node_2 = node_2.next.next
+        if node_1 == node_2:
+            return True
+
+    return False
