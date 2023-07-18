ae35020fca0ad9fe2af68b5be606e64efc9e101c
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-07-18 14:00:26 -0700

    changed text

---
+++ Array/next_permutation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 1
+        while i > 0 and nums[i - 1] >= nums[i]:
+            i = i - 1
+        if i < 1:
+            nums[:] = nums[::-1]
+        else:
+            j = len(nums) - 1  # last element
+            # we have to find element from last that is greater than nums[i-1]
+            while nums[j] <= nums[i - 1]:
+                j = j - 1
+            nums[i - 1], nums[j] = nums[j], nums[i - 1]
+            nums[i:] = reversed(nums[i:])
