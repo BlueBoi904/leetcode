ef6c2d292664c9787d88a30aaaa50f3ceabea86c
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-08-19 16:51:58 -0700

    got daily solutions

---
+++ Array/find_smaller_letter_grade.py
@@ -0,0 +1,7 @@
+class Solution:
+    def nextGreatestLetter(self, letters: list[str], target: str) -> str:
+        for letter in letters:
+            if letter > target:
+                return letter
+
+        return letters[0]
