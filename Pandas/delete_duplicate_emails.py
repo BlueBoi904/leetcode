d02c37b2c5e783b52604eb5406aaf5b9a21f465a
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-08-21 11:02:28 -0700

    got daily solutions

--- Pandas/delete_duplicate_emails.py
+++ Pandas/delete_duplicate_emails.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 # Modify Person in place
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
-    person.sort_values(["id"], ascending=True, inplace=True)
-    person.drop_duplicates(["email"], inplace=True)
+    person.sort_values(by=['id'], inplace=True)
+    person.drop_duplicates(subset='email', keep="first", inplace=True)
