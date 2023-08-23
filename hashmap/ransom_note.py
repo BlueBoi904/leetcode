040ab89438bf2cada48c6a77953b6bb107c9198a
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-08-23 16:57:12 -0700

    Added solutions, got another!!!

---
+++ hashmap/ransom_note.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+
+def ransom_note(ransom_note: str, magazine: str):
+    mag = Counter(magazine)
+    note = Counter(ransom_note)
+    return all(mag[c] >= note[c] for c in note)
+
+
+
+print(ransom_note("a", "b"))
+
+print(ransom_note("aa", "ab"))
+
+print(ransom_note("aa", "aab"))
