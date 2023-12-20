86c6d3150120a6a4267cdd981d5a6e7bfbb0a47b
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-12-19 17:21:29 -0700

    Added python solutions

--- Daily/is_sub.py
+++ Daily/is_sub.py
@@ -1,11 +1,11 @@
-class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        count = 0
-        size = len(s)
-        for x in t:
-            if (count == size):
-                break
-            if (x == s[count]):
-                count += 1
+def is_subsequence(s: str, t: str) -> bool:
+    count = 0
+    size = len(s)
+    for x in t:
+        if count == size:
+            break
+        if x == s[count]:
+            count += 1
 
-        return size == count
+    return size == count
+
